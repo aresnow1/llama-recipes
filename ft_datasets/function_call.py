7ab302467e396713143d4cfe305e1d14f75600ec
aresnow <aresnow1@gmail.com>
2023-08-08 18:24:37 +0800

    define map function

--- ft_datasets/function_call.py
+++ ft_datasets/function_call.py
@@ -1,18 +1,44 @@
+import copy
 
 import datasets
-from .utils import Concatenator
+import torch
 
 PROMPT = f"{{sys_prompt}} Response:{{response}}"
 
 
 def get_preprocessed_function_call(dataset_config, tokenizer, split):
     dataset = datasets.load_dataset("marclove/llama_functions", split="train")
-    dataset = dataset.train_test_split(test_size=0.05)[split]
+    if split == "test":
+        dataset = dataset.train_test_split(test_size=0.1)[split]
+
+    def convert(sample):
+        max_words = 4096
+        full = tokenizer.encode(sample["input"] + sample["output"])
+        prompt = torch.tensor(
+            tokenizer.encode(sample["input"]), dtype=torch.int64
+        )
+        full = torch.tensor(full + [tokenizer.eos_token_id], dtype=torch.int64)
+        padding = max_words - full.shape[0]
+        if padding > 0:
+            full = torch.cat((full, torch.zeros(padding, dtype=torch.int64) - 1))
+        elif padding < 0:
+            full = full[: max_words]
+        labels = copy.deepcopy(full)
+        labels[: len(prompt)] = -1
+        full_mask = full.ge(0)
+        label_mask = labels.ge(0)
+        full[~full_mask] = 0
+        labels[~label_mask] = 0
+        full_mask = full_mask.float()
+        label_mask = label_mask.float()
+        return {
+            "input_ids": full,
+            "labels": labels,
+            "attention_mask": full_mask,
+        }
 
     dataset = dataset.map(
-        lambda sample: tokenizer(sample["input"] + sample["output"] + tokenizer.eos_token),
-        batched=True,
+        convert,
         remove_columns=list(dataset.features),
     )
-    dataset = dataset.map(Concatenator(chunk_size=512), batched=True)
     return dataset
